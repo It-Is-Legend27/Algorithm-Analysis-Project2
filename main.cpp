3e23efcdb6eda1977fc693a384ea000a41b893d5
Angel Badillo Hernandez <badilloa022402@gmail.com>
2024-02-25 17:04:57 -0600

    working on project

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <string>
+#include <chrono>
 #include "sorts.hpp"
 using namespace std;
 
-void init_random(int arr[], int n)
+const int ARR_SIZE = 10;
+
+string array_to_string(int arr[], int n)
 {
+    string repr = "";
+
+    repr += "[";
+
     for (int i = 0; i < n; i++)
     {
-        arr[i] = random() % 10;
+        if (i != n - 1)
+            repr += to_string(arr[i]) + ", ";
+        else
+            repr += to_string(arr[i]);
     }
-    
+
+    repr += "]";
+    return repr;
 }
 
 int main()
@@ -18,5 +31,99 @@ int main()
     // Seeding random number generator
     srand(time(NULL));
 
-    // Stuff
+    int *S1 = new int[ARR_SIZE]; // Insertion Sort array
+    int *S2 = new int[ARR_SIZE]; // Selection Sort array
+    int *S3 = new int[ARR_SIZE]; // Merge Sort array
+    int *S4 = new int[ARR_SIZE]; // Quick Sort array
+
+    long time1;
+    long time2;
+    long time3;
+    long time4;
+
+    int c1; // Counter for Insertion Sort
+    int c2; // Counter for Selection Sort
+    int c3; // Counter for Merge Sort
+    int c4; // Counter for Quick Sort
+
+    
+
+    // Averaging results over 10 runs
+    for (int runID = 1; runID <= 1; runID++)
+    {
+        c1 = c2 = c3 = c4 = 0; // Intialize counters to 0
+        time1 = time2 = time3 = time4 = 0; // Intialize times to 0
+
+        // Intializing arrays
+        for (int i = 0; i < ARR_SIZE; i++)
+        {
+            int x = rand();
+            S1[i] = x;
+            S2[i] = x;
+            S3[i] = x;
+            S4[i] = x;
+        }
+
+        auto start_time = chrono::high_resolution_clock::now();
+        c1 = insertion_sort(S1, ARR_SIZE);
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+        time1 = duration.count();
+
+        start_time = chrono::high_resolution_clock::now();
+        c2 = selectionSort(S2, ARR_SIZE);
+        end_time = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+        time2 = duration.count();
+
+        start_time = chrono::high_resolution_clock::now();
+        mergeSort(S3, 0, ARR_SIZE);
+        end_time = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+        time3 = duration.count();
+
+        start_time = chrono::high_resolution_clock::now();
+        quickSort(S4, 0, ARR_SIZE);
+        end_time = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+        time4 = duration.count();
+
+        cout << "Run " << runID << "\n";
+        cout << string(40, '*') << "\n";
+
+        cout << "Selection Sort: ";
+        //cout << array_to_string(S1, ARR_SIZE);
+        cout << "\n";
+        cout << "Counter: " << c1 << "\n";
+        cout << "Time: " << time1 << "\n\n";
+
+        cout << "Insertion Sort: ";
+        //cout << array_to_string(S2, ARR_SIZE);
+        cout << "\n";
+        cout << "Counter: " << c2 << "\n";
+        cout << "Time: " << time2 << "\n\n";
+
+        cout << "Merge Sort:     ";
+        //cout << array_to_string(S3, ARR_SIZE);
+        cout << "\n";
+        cout << "Counter: " << c3 << "\n";
+        cout << "Time: " << time3 << "\n\n";
+
+        cout << "Quick Sort:     ";
+        //cout << array_to_string(S4, ARR_SIZE);
+        cout << "\n";
+        cout << "Counter: " << c4 << "\n";
+        cout << "Time: " << time4 << "\n\n";
+        
+        cout << string(40, '*') << "\n";
+        cout << "\n";
+    }
+
+    // Free memory
+    delete[] S1;
+    delete[] S2;
+    delete[] S3;
+    delete[] S4;
+
+    return 0;
 }
--- sorts.hpp
+++ sorts.hpp
@@ -1,51 +1,80 @@
 #ifndef PROJECT2_SORTS
 #define PROJECT2_SORTS
 #include <iostream>
+#include <algorithm> // For swap function
 using namespace std;
 
-// Source: https://www.geeksforgeeks.org/insertion-sort/
-void insertion_sort(int arr[], int n)
+/**
+ * @brief Performs insertion sort on an unsorted array of integers.
+ * Source: https://www.geeksforgeeks.org/insertion-sort/
+ * @param arr Array of int
+ * @param n Size of array
+ * @return int - Number of comparisons performed
+ */
+int insertion_sort(int arr[], int n)
 {
+    int comp_counter = 0; // Counter for comparisons
+
     int i, key, j;
-    for (i = 1; i < n; i++) {
+    for (i = 1; i < n; i++)
+    {
         key = arr[i];
         j = i - 1;
- 
+
         // Move elements of arr[0..i-1],
-        // that are greater than key, 
+        // that are greater than key,
         // to one position ahead of their
         // current position
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
+            comp_counter++; // Increment counter
+
             arr[j + 1] = arr[j];
             j = j - 1;
         }
         arr[j + 1] = key;
     }
+
+    return comp_counter;
 }
 
-// Source: https://www.geeksforgeeks.org/selection-sort/
-void selectionSort(int arr[], int n) 
-{ 
-    int i, j, min_idx; 
-  
-    // One by one move boundary of 
-    // unsorted subarray 
-    for (i = 0; i < n - 1; i++) { 
-  
-        // Find the minimum element in 
-        // unsorted array 
-        min_idx = i; 
-        for (j = i + 1; j < n; j++) { 
-            if (arr[j] < arr[min_idx]) 
-                min_idx = j; 
-        } 
-  
-        // Swap the found minimum element 
-        // with the first element 
-        if (min_idx != i) 
-            swap(arr[min_idx], arr[i]); 
-    } 
-} 
+/**
+ * @brief Perforns selection sort on an unsorted array of integers.
+ * Source: https://www.geeksforgeeks.org/selection-sort/
+ * @param arr Array of int
+ * @param n Size of array
+ * @return int = Number of comparisons performed
+ */
+int selectionSort(int arr[], int n)
+{
+    int comp_counter = 0; // Counter for comparisons
+
+    int i, j, min_idx;
+
+    // One by one move boundary of
+    // unsorted subarray
+    for (i = 0; i < n - 1; i++)
+    {
+
+        // Find the minimum element in
+        // unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        {
+            comp_counter++; // Increment counter
+
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+        // Swap the found minimum element
+        // with the first element
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
+
+    return comp_counter;
+}
 
 // Merges two subarrays of array[].
 // First subarray is arr[begin..mid]
@@ -56,58 +85,57 @@ void merge(int array[], int const left, int const mid,
 {
     int const subArrayOne = mid - left + 1;
     int const subArrayTwo = right - mid;
- 
+
     // Create temp arrays
     auto *leftArray = new int[subArrayOne],
          *rightArray = new int[subArrayTwo];
- 
+
     // Copy data to temp arrays leftArray[] and rightArray[]
     for (auto i = 0; i < subArrayOne; i++)
         leftArray[i] = array[left + i];
     for (auto j = 0; j < subArrayTwo; j++)
         rightArray[j] = array[mid + 1 + j];
- 
+
     auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
     int indexOfMergedArray = left;
- 
+
     // Merge the temp arrays back into array[left..right]
-    while (indexOfSubArrayOne < subArrayOne
-           && indexOfSubArrayTwo < subArrayTwo) {
-        if (leftArray[indexOfSubArrayOne]
-            <= rightArray[indexOfSubArrayTwo]) {
-            array[indexOfMergedArray]
-                = leftArray[indexOfSubArrayOne];
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
             indexOfSubArrayOne++;
         }
-        else {
-            array[indexOfMergedArray]
-                = rightArray[indexOfSubArrayTwo];
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
             indexOfSubArrayTwo++;
         }
         indexOfMergedArray++;
     }
- 
+
     // Copy the remaining elements of
     // left[], if there are any
-    while (indexOfSubArrayOne < subArrayOne) {
-        array[indexOfMergedArray]
-            = leftArray[indexOfSubArrayOne];
+    while (indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
         indexOfSubArrayOne++;
         indexOfMergedArray++;
     }
- 
+
     // Copy the remaining elements of
     // right[], if there are any
-    while (indexOfSubArrayTwo < subArrayTwo) {
-        array[indexOfMergedArray]
-            = rightArray[indexOfSubArrayTwo];
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
         indexOfSubArrayTwo++;
         indexOfMergedArray++;
     }
     delete[] leftArray;
     delete[] rightArray;
 }
- 
+
 // begin is for left index and end is right index
 // of the sub-array of arr to be sorted
 // Source: https://www.geeksforgeeks.org/merge-sort/
@@ -115,54 +143,54 @@ void mergeSort(int array[], int const begin, int const end)
 {
     if (begin >= end)
         return;
- 
+
     int mid = begin + (end - begin) / 2;
     mergeSort(array, begin, mid);
     mergeSort(array, mid + 1, end);
     merge(array, begin, mid, end);
 }
 
-// Source: https://www.geeksforgeeks.org/quick-sort/ 
-int partition(int arr[],int low,int high)
+// Source: https://www.geeksforgeeks.org/quick-sort/
+int partition(int arr[], int low, int high)
 {
-  //choose the pivot
-   
-  int pivot=arr[high];
-  //Index of smaller element and Indicate
-  //the right position of pivot found so far
-  int i=(low-1);
-   
-  for(int j=low;j<=high;j++)
-  {
-    //If current element is smaller than the pivot
-    if(arr[j]<pivot)
+    // choose the pivot
+
+    int pivot = arr[high];
+    // Index of smaller element and Indicate
+    // the right position of pivot found so far
+    int i = (low - 1);
+
+    for (int j = low; j <= high; j++)
     {
-      //Increment index of smaller element
-      i++;
-      swap(arr[i],arr[j]);
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            // Increment index of smaller element
+            i++;
+            swap(arr[i], arr[j]);
+        }
     }
-  }
-  swap(arr[i+1],arr[high]);
-  return (i+1);
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
 }
- 
+
 // The Quicksort function Implement
-// Source: https://www.geeksforgeeks.org/quick-sort/            
-void quickSort(int arr[],int low,int high)
+// Source: https://www.geeksforgeeks.org/quick-sort/
+void quickSort(int arr[], int low, int high)
 {
-  // when low is less than high
-  if(low<high)
-  {
-    // pi is the partition return index of pivot
-     
-    int pi=partition(arr,low,high);
-     
-    //Recursion Call
-    //smaller element than pivot goes left and
-    //higher element goes right
-    quickSort(arr,low,pi-1);
-    quickSort(arr,pi+1,high);
-  }
+    // when low is less than high
+    if (low < high)
+    {
+        // pi is the partition return index of pivot
+
+        int pi = partition(arr, low, high);
+
+        // Recursion Call
+        // smaller element than pivot goes left and
+        // higher element goes right
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
 #endif
